0549039ab7276e28a3e287977c27eee9053fb61e
Fatih Furkan Arslan <arslanfatihfurkan@gmail.com>
2024-09-26 14:00:15 +0300

    going on skelaton process

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //Business logic to implement creating a product 
+            throw new NotImplementedException();
+        }
+    }
+}
